1ce9c0b9d54144fa6adab669ad41fd142a49e2bf
Filip <filip.trajanovski@student.nbi-handelsakademin.se>
2023-11-13 14:31:15 +0100

    Task 3!

--- FizzBuzz/Kalkylator.cs
+++ FizzBuzz/Kalkylator.cs
@@ -4,14 +4,20 @@
     {
         public static string FizzBuzzKalkyl(int nummer)
         {
-            if(nummer % 3 == 0)
+            if (nummer % 3 == 0)
             {
                 return "FIZZ";
             }
             else if (nummer % 5 == 0)
             {
-               return "BUZZ";
+                return "BUZZ";
             }
+            else if (nummer % 3 == 0 && nummer % 5 == 0)
+            {
+                return "FizzBuzz";
+
+            }
+
             else
             {
                 return nummer.ToString();
