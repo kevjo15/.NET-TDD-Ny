7eb04b44c4163868ba0508f83e9f08c8d118159b
Kevin <114003395+kevjo15@users.noreply.github.com>
2023-11-13 14:11:45 +0100

    2

--- FizzBuzz/Kalkylator.cs
+++ FizzBuzz/Kalkylator.cs
@@ -2,7 +2,7 @@
 {
     public class Kalkylator
     {
-        public static void FizzBuzzKalkyl(int nummer)
+        public static string FizzBuzzKalkyl(int nummer)
         {
             if(nummer % 3 == 0)
             {
